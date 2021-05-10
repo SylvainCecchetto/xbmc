b87da974e7eb644e7ec18f586ca975b1ade6456e
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2021-05-10 13:22:53 +0200

    [tvOS/Remote] Add missing Siri remote press types

--- xbmc/platform/darwin/tvos/input/LibInputTouch.mm
+++ xbmc/platform/darwin/tvos/input/LibInputTouch.mm
@@ -259,18 +259,32 @@
 
 - (void)longPlayPausePressed:(UILongPressGestureRecognizer*)sender
 {
-  CLog::Log(LOGDEBUG, "Input: play/pause long press, state: %ld", static_cast<long>(sender.state));
+  if (sender.state == UIGestureRecognizerStateBegan)
+  {
+    CLog::Log(LOGDEBUG, "Input: Siri remote play/pause long press (id: 20)");
+    [g_xbmcController.inputHandler sendButtonPressed:20];
+    [g_xbmcController.inputHandler.inputRemote startSiriRemoteIdleTimer];
+  }
 }
 
 - (void)doublePlayPausePressed:(UITapGestureRecognizer*)sender
 {
-  // state is only UIGestureRecognizerStateBegan and UIGestureRecognizerStateEnded
-  CLog::Log(LOGDEBUG, "Input: play/pause double press");
+  if (sender.state == UIGestureRecognizerStateEnded)
+  {
+    CLog::Log(LOGDEBUG, "Input: Siri remote play/pause double press (id: 21)");
+    [g_xbmcController.inputHandler sendButtonPressed:21];
+    [g_xbmcController.inputHandler.inputRemote startSiriRemoteIdleTimer];
+  }
 }
 
 - (void)SiriDoubleSelectHandler:(UITapGestureRecognizer*)sender
 {
-  CLog::Log(LOGDEBUG, "Input: select double press");
+  if (sender.state == UIGestureRecognizerStateEnded)
+  {
+    CLog::Log(LOGDEBUG, "Input: Siri remote select double press (id: 22)");
+    [g_xbmcController.inputHandler sendButtonPressed:22];
+    [g_xbmcController.inputHandler.inputRemote startSiriRemoteIdleTimer];
+  }
 }
 
 #pragma mark - IR Arrows Pressed
